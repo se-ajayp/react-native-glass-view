f5d98d796849241e8d4b6dbaad66409196eb9f16
software-engineer-ajay <ajaypanchal_27@yahoo.in>
2024-04-12 10:11:33 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const GlassView = ({ children }) => {
+    return (
+        <View style={styles.container}>
+            <BlurView
+                style={styles.blur}
+                blurType="light"
+                blurAmount={10} // Adjust blur amount as needed
+            >
+                {children}
+            </BlurView>
+        </View>
+    )
+}
+
+export default GlassView
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.3)', // Semi-transparent background
+        borderRadius: 10, // Optional: Add border radius for rounded corners
+    },
+    blur: {
+        flex: 1,
+        borderRadius: 10, // Optional: Same border radius as container
+    },
+})
