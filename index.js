c9f7e392906376e7602b974a4174a48aebed2676
software-engineer-ajay <ajaypanchal_27@yahoo.in>
2024-04-12 11:11:42 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import { BlurView } from '@react-native-community/blur';
 const GlassView = ({ children }) => {
     return (
         <View style={styles.container}>
